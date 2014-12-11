8c706f7aa86efc5b99c774aa2148d3d8fa128cd0
Neil Edwards <neil@plasticwater.com>
2014-12-10 21:38:21 -0500

    fix tests

--- test/controllers/picked_controller_test.rb
+++ test/controllers/picked_controller_test.rb
@@ -1,9 +1,5 @@
 require "test_helper"
 
 class PickedControllerTest < ActionController::TestCase
-  def test_create
-    get :create
-    assert_response :success
-  end
 
 end
--- test/helpers/picked_helper_test.rb
+++ test/helpers/picked_helper_test.rb
@@ -2,8 +2,5 @@ require "test_helper"
 
 class PickedHelperTest < ActionView::TestCase
 
-  def test_sanity
-    flunk "Need real tests"
-  end
 
 end
