2a71b22b8358ba4436184b2e4c9d705aac3fd27b
Neil Edwards <neil@plasticwater.com>
2015-01-26 22:53:00 -0500

    added helper for urls

--- app/helpers/talent_helper.rb
+++ app/helpers/talent_helper.rb
@@ -1,2 +1,6 @@
 module TalentHelper
+
+  def get_profile_picture(talent, style=:thumb)
+    talent.photos.any? ? "<img src=\"#{talent.photos.first.url(style)}\">" : '<img data-src="holder.js/150x150">'
+  end
 end
--- test/helpers/talent_helper_test.rb
+++ test/helpers/talent_helper_test.rb
@@ -1,4 +1,20 @@
 require 'test_helper'
 
 class TalentHelperTest < ActionView::TestCase
+  def talent
+    Talent.new
+  end
+
+  test 'talent with no photos gets a helper placeholder' do
+    assert_equal '<img data-src="holder.js/150x150">', get_profile_picture(talent)
+  end
+
+  test 'talent with one photo shows that photo' do
+    t = talent
+    p = Photo.new
+    t.photos << p
+    p.stub :url, 'test_url' do
+      assert_equal '<img src="test_url">', get_profile_picture(t)
+    end
+  end
 end
