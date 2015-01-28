4d37a02785cacc616b264797bd7eb3e5dbaafd7a
Neil Edwards <neil@plasticwater.com>
2015-01-28 15:54:15 -0500

    add minitest mock

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "capybara/rails"
 require 'minitest/pride'
+require 'minitest/mock'
 
 class ActionDispatch::IntegrationTest
   include Capybara::DSL
